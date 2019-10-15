f998ea06a8f89c2a1f5ed820f773e44479ae8dd7
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-10-15 17:57:00 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "fila_duplamente_encadeada.h"
+
+void aloca_fila(TipoFila fila){
+
+}
+
+int main() {
+    TipoItem item1,item2,item3,item4,item5;
+    TipoFila fila;
+    FFVazia(&fila);
+    item1.Chave = 1;
+    Enfileira(item1, &fila);
+    item2.Chave = 2;
+    Enfileira(item2, &fila);
+    item3.Chave = 3;
+    Enfileira(item3, &fila);
+    item4.Chave = 4;
+    Enfileira(item4, &fila);
+    item5.Chave = 5;
+    Enfileira(item5, &fila);
+
+    printf("=================\n");
+    Imprime(fila);
+    AumentaPrioridade(&fila,&item2);
+    Desenfileira(&fila,&item4);
+    printf("=================\n");
+    Imprime(fila);
+
+
+
+    return 0;
+}
